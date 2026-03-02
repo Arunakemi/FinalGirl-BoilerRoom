d35817f449fb6897b7983eaffd1f83a29ec3c446
Arunakemi-GridMedia <42361534+Arunakemi@users.noreply.github.com>
2026-03-02 15:18:47 -0600

    Check for valid movements

--- source/main.ts
+++ source/main.ts
@@ -1,4 +1,8 @@
-// Author: Arunakemi c:
+/* **************************************************************************************************
+        AUTHOR: @Arunakemi   
+        LAST UPDATE: 02/March/2026
+    ************************************************************************************************* */ 
+
 
 // Fisher-Yates Shuffle
 // Taken from: https://stackoverflow.com/questions/59810241/how-to-fisher-yates-shuffle-a-javascript-array
@@ -12,24 +16,34 @@ const Shuffle = (arr : any[]) => {
   }
 }
 // Utils
+const DIRS = [
+    { x: 0, y: 1},
+    { x: -1, y: 0},
+    { x: 0, y: -1},
+    { x: 1, y: 0},
+]
 const Hide = (e : HTMLElement)=>{
     e.style.visibility = 'hidden'
 }
 const Show = (e : HTMLElement)=>{
     e.style.visibility = 'visible'
 }
-
+const newCoordElem = (e : HTMLElement) => ({
+    element: e,
+    x: 0,
+    y: 0,
+})
 
 // HTML References
-let cardSleep = [document.getElementById("cardSleep") as HTMLImageElement, 0, 0]
+let cardSleep = newCoordElem(document.getElementById("cardSleep") as HTMLImageElement)
 let cardAwake = document.getElementById("cardAwake") as HTMLImageElement
-cardAwake.style.zIndex = '9000'
-;(cardSleep[0] as HTMLImageElement).style.zIndex = '9000'
+cardAwake.style.zIndex = '9500'
+cardSleep.element.style.zIndex = '9000'
 let cards = [
-    [document.getElementById("card01") as HTMLImageElement, 0, 0],
-    [document.getElementById("card02") as HTMLImageElement, 0, 0],
-    [document.getElementById("card03") as HTMLImageElement, 0, 0],
-    [document.getElementById("card04") as HTMLImageElement, 0, 0]
+    newCoordElem(document.getElementById("card01") as HTMLImageElement),
+    newCoordElem(document.getElementById("card02") as HTMLImageElement),
+    newCoordElem(document.getElementById("card03") as HTMLImageElement),
+    newCoordElem(document.getElementById("card04") as HTMLImageElement)
 ]
 let bttnDir = [ 
     document.getElementById("bttnUp") as HTMLButtonElement, 
@@ -44,17 +58,17 @@ let cardIndex = 0
 let lastDir = 0
 const TransformCards = ()=>{
     console.log('transforming')
-    ;(cardSleep[0] as HTMLImageElement).style.transform = `translate(${(cardSleep[1] as number)*50}%,${(cardSleep[2] as number)*50}%)`
+    cardSleep.element.style.transform = `translate(${cardSleep.x * 50}%,${cardSleep.y * 50}%)`
     for(let i=0; i<cards.length; i++)
     { 
-        ;(cards[i][0] as HTMLImageElement).style.transform = `translate(${(cards[i][1] as number)*50}%,${(cards[i][2] as number)*50}%)` 
+        cards[i].element.style.transform = `translate(${cards[i].x * 50}%,${cards[i].y * 50}%)` 
     }
 }
 const Sleep = () => {
 
     Shuffle(cards)
     for(let i=0; i<cards.length; i++)
-    { (cards[i][0] as HTMLImageElement).style.zIndex = 1000 - i * 100 + '' }
+    { cards[i].element.style.zIndex = 1000 - i * 100 + '' }
     cardIndex = 0
     lastDir = 0
 
@@ -62,51 +76,64 @@ const Sleep = () => {
     Hide(cardAwake)
     for(let i=0;i<bttnDir.length;i++)
     { Show(bttnDir[i]) }
-    Show(cardSleep[0] as HTMLImageElement)
+    Show(cardSleep.element)
+
+    for(let i=0;i<bttnDir.length;i++)
+    {
+        bttnDir[i].disabled = false
+    }
 }
 const WakeUp = () => {
 
-    cardSleep[1] = 0
-    cardSleep[2] = 0
+    cardSleep.x = 0
+    cardSleep.y = 0
     for(let i=0; i<cards.length; i++)
     { 
-        cards[i][1] = 0 
-        cards[i][2] = 0 
+        cards[i].x = 0 
+        cards[i].y = 0 
     }
     TransformCards()
 
-    Hide(cardSleep[0] as HTMLImageElement)
+    Hide(cardSleep.element)
     Hide(bttnWake)
 
     Show(bttnSleep)
     Show(cardAwake)
 }
-const Move = (dir : number)=>{
-
-    const odd = dir % 2 == 0
-    let x = odd? 0 : (dir - 2)
-    let y = odd? (dir - 1)*-1 : 0
-    ;(cardSleep[1] as number) += x
-    ;(cardSleep[2] as number) += y
+const CheckifInvalid = (dir : {x:number,y:number}) => {
+    console.log('checking')
+    let isInvalid = cardSleep.x + dir.x == 0 && cardSleep.y + dir.y == 0
     for(let i=0; i<cardIndex; i++)
     {
-        ;(cards[i][1] as number) += x
-        ;(cards[i][2] as number) += y
+        isInvalid = isInvalid || (cards[i].x + dir.x == 0 && cards[i].y + dir.y == 0)
+    }
+    return isInvalid
+}
+
+const Move = (dir : number)=>{
+
+    let x = DIRS[dir].x
+    let y = DIRS[dir].y
+
+    cardSleep.x += x
+    cardSleep.y += y
+    for(let i=0; i<cardIndex; i++)
+    {
+        cards[i].x += x
+        cards[i].y += y
     }
     TransformCards()
 
-    bttnDir[lastDir].disabled = false
-    let disableDir = dir - 2 < 0? dir + 2 : dir -2
-    console.log(disableDir)
-    bttnDir[disableDir].disabled = true
-    lastDir = disableDir 
+    for(let i=0;i<bttnDir.length;i++)
+    {
+        bttnDir[i].disabled = CheckifInvalid(DIRS[i])
+    }
 
     cardIndex++
     if(cardIndex >= cards.length)
     {
         for(let i=0;i<bttnDir.length;i++)
         { Hide(bttnDir[i]) }
-        bttnDir[lastDir].disabled = false
         Show(bttnWake)
     }
 }
